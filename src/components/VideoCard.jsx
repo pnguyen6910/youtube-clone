353fe84c1e6afa42bbd2c9a451238daac69e5a03
Paul Nguyen <pnguyen6910@gmail.com>
2022-10-14 11:53:53 -0500

    styled Card to fit better on page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,7 +6,7 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   console.log(videoId, snippet)
   return (
-    <Card>
+    <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }} >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia 
         image={snippet.thumbnails?.high?.url} 
