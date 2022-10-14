7816d20ee58963f95f692195de09d58228029266
Paul Nguyen <pnguyen6910@gmail.com>
2022-10-14 14:29:07 -0500

    profile pictures showing correctly on channel page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -2,7 +2,6 @@ import { Box, CardContent, CardMedia, Typography } from "@mui/material";
 import { CheckCircle } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
-import { flexbox } from "@mui/system";
 
 const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
@@ -29,9 +28,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
         }}
       >
         <CardMedia
-          image={
-            channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
-          }
+          image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
           alt={channelDetail?.snippet?.title}
           sx={{
             borderRadius: "50%",
@@ -42,7 +39,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
           }}
         />
         <Typography variant="h6">
-          {channelDetail?.snippet?.title}
+          {channelDetail?.snippet?.title}{' '}
           <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
         </Typography>
         {channelDetail?.statistics?.subscriberCount && (
