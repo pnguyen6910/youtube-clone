a914d01fd354fe503e565b13a90091139f5831ed
Paul Nguyen <pnguyen6910@gmail.com>
2022-10-14 14:37:10 -0500

    starting video detail page

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Videos, Loader } from "./";
+import { fetchFromAPI } from "../utils/fetchfromAPI";
 
 const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null)
+  const { id } = useParams
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) => setVideoDetail(data.item[0]))
+  }, [id])
+  
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight= "95vh">
+      <Stack direction= {{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
